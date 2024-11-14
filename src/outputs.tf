568355ef9809a37720804e462fd95943c2bd3add
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:20:12 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,9 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "dms_replication_instance_id" {
+  value       = module.dms_replication_instance.replication_instance_id
+  description = "DMS replication instance ID"
+}
+
+output "dms_replication_instance_arn" {
+  value       = module.dms_replication_instance.replication_instance_arn
+  description = "DMS replication instance ARN"
 }
